e4cc8bc3a8d4d877283ac11c5d8c51454e9f8d42
Wander Nauta <info@wandernauta.nl>
2014-04-22 19:48:30 +0200

    Clean up LinkLayerReceiver

--- src/link/LinkLayerReceiver.java
+++ src/link/LinkLayerReceiver.java
@@ -4,17 +4,12 @@ import lpt.Lpt;
 
 public class LinkLayerReceiver {
 	public static void main(String[] args) {
-        Lpt lpt = new Lpt();
         boolean loop_continue = true;
-        int oldIn = 0;
         LinkLayer linkLayer = new SingleDirectionLinkLayer(new Lpt());
-        byte oldByte = 1;
-        
         
         while(loop_continue) {
         	byte newByte = linkLayer.readByte();
             System.out.println((int)newByte);
-            oldByte = newByte;
         }
     }
 }
