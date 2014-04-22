7c9c5af55247dcd4b66453b185d83bed81ddcc4e
Jan-Jelle Kester <janjelle@jjkester.nl>
2014-04-22 12:47:52 +0200

    Use output format from Derks receiver

--- src/test/official/Receiver.java
+++ src/test/official/Receiver.java
@@ -6,10 +6,15 @@ public class Receiver {
     public static void main(String[] args) {
         Lpt lpt = new Lpt();
         boolean loop_continue = true;
+        int oldIn = 0;
 
         while(loop_continue) {
             int in = lpt.readLPT();
-            System.out.println(in);
+            oldIn = in;
+
+            if (in != oldIn) {
+                System.out.println(in + " \t=\t" + Integer.toBinaryString(in));
+            }
         }
     }
 }
