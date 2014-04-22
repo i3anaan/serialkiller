95953bd64370ca6cf836e2fb2e89f3d9e544ab2c
Wander Nauta <info@wandernauta.nl>
2014-04-22 19:56:39 +0200

    Add NullLinkLayer, a noop link layer

---
+++ src/link/NullLinkLayer.java
@@ -0,0 +1,20 @@
+package link;
+
+/**
+ * A null implementation of a link layer. Reading from this link layer will
+ * immediately return null (0) bytes, and writing to it will do precisely
+ * nothing.
+ */
+public class NullLinkLayer implements LinkLayer {
+	@Override
+	public void sendByte(byte data) {
+		/* Do nothing. */
+	}
+
+	@Override
+	public byte readByte() {
+		/* Pretend we've read a byte. */
+		return 0;
+	}
+
+}
