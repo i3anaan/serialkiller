8157213c75350cef27f6387ede4d9085e52bacbb
I3anaan <I3anaan@live.nl>
2014-04-22 10:20:17 +0200

    Better send/receive test class

--- test/TestSendJunk.java
+++ test/TestSendJunk.java
@@ -1,16 +1,37 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+
+import lpt.Lpt;
 
 public class TestSendJunk {
 	public static void main(String[] args) throws IOException {
-		OutputStream os = new FileOutputStream(new File("/telpparport"));
+		if(args.length==1){
 		
-		while (true) {
-			for (int i = 0; i < Byte.MAX_VALUE; i++) {
-				os.write(i);
+			OutputStream os = new FileOutputStream(new File("/telpparport"));
+			InputStream is = new FileInputStream(new File("/telpparport"))
+			
+			switch(args[0]){
+			case "spam":
+				
+				
+				while (true) {
+					for (int i = 0; i < Byte.MAX_VALUE; i++) {
+						os.write(i);
+						os.flush();
+					}
+				}
+				break;
+			case "sendint":
+				os.write(123);
 				os.flush();
+				break;
+			case "receiveint":
+				String intRead = is.read();
+				System.out.println(intRead);				
 			}
 		}
 	}
