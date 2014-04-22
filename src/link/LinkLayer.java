f7ce87984a9ae9b5bf3c0a3b24c65e2169a24c99
Wander Nauta <info@wandernauta.nl>
2014-04-22 19:48:48 +0200

    Clean up LinkLayer

--- src/link/LinkLayer.java
+++ src/link/LinkLayer.java
@@ -1,7 +1,5 @@
 package link;
 
-import lpt.Lpt;
-
 /**
  * Basic interface for implementations of link layers. This interface specifies the basic API to ensure compatibility
  * independent of the type of link layer that is used.
