0cba4a3017163f81b80f2917f2fa891132671c75
I3anaan <I3anaan@live.nl>
2014-04-25 15:55:15 +0200

    Poging 2 te committen

--- src/link/HighSpeedHDXLinkLayer.java
+++ src/link/HighSpeedHDXLinkLayer.java
@@ -2,28 +2,33 @@ package link;
 
 import phys.HardwareLayer;
 import phys.PhysicalLayer;
+import util.Bytes;
 
 public class HighSpeedHDXLinkLayer extends LinkLayer {
 
 	byte expectedAck = Byte.MAX_VALUE;
-	byte oldBitSend;
-	byte oldBitReceived;
+	byte oldByteSend;
+	byte oldByteReceived;
 	boolean connectionSetup;
+	boolean startWithByte = true;
 
 	public HighSpeedHDXLinkLayer(PhysicalLayer phys) {
 		super();
 		this.down = phys;
-		oldBitSend = 0;
+		oldByteSend = 0;
 		// Previous input has to be 0;
-		oldBitReceived = down.readByte();
-		down.sendByte((byte)0); //Used to make sure both sides start at correct state;
+		oldByteReceived = down.readByte();
+		down.sendByte((byte) 0); // Used to make sure both sides start at
+									// correct state;
+		System.out.println("Send 0 byte");
 		connectionSetup = false;
 	}
 
 	@Override
 	public void sendByte(byte data) {
-		// TODO sync before the very first byte sent (put both ends on 0)
-
+		byte fullDataToSend = data;
+		System.out.println("#######>"+this.hashCode()+"   START SENDING BYTE: "
+				+ Bytes.format(data));
 		int bitIndex = 0;
 		// For every bit in the byte...
 		while (bitIndex < 8) {
@@ -31,13 +36,14 @@ public class HighSpeedHDXLinkLayer extends LinkLayer {
 			byte databit = (byte) (data & 1);
 			data >>= 1;
 			bitIndex++;
-			boolean databitChanged = databit != ((byte) (oldBitSend & 1));
-			byte extrabit; // Stores the extra bit, on the correct position (ie,
-							// is 0 or 2)
+			boolean databitChanged = databit != ((byte) (oldByteSend & 1));
+			byte extrabit = 0; // Stores the extra bit, on the correct position
+								// (ie,
+								// is 0 or 2)
 
 			if (!databitChanged) {
 				// Send 1 data bit;
-				if ((oldBitSend & 2) == 2) { // Extra bit was 1
+				if ((oldByteSend & 2) == 2) { // Extra bit was 1
 					extrabit = 0;
 				} else {
 					extrabit = 2;
@@ -53,24 +59,29 @@ public class HighSpeedHDXLinkLayer extends LinkLayer {
 				}
 			}
 
-			// Wait for the acknowledgement of the previous bit...
-			while (expectedAck != Byte.MAX_VALUE
-					&& down.readByte() != expectedAck) {
-				// Waiting till expectedAck is received;
-			}
-
 			// Pack it with the clock and send it.
 			byte output = (byte) (0 | extrabit | databit);
 			down.sendByte(output);
-
-			expectedAck = HardwareLayer.shuftLeft(output);
-
-			data >>= 1; // Shift the sent bit off.
+			oldByteSend = output;
+			byte currentIn = down.readByte();
+			// Wait for the acknowledgement of the previous bit...
+			while (currentIn == oldByteSend) {
+				// System.out.println("Awaiting ack, receiving: "+Bytes.format(down.readByte())+"  Expecting: "+Bytes.format(oldBitSend));
+				// Waiting till expectedAck is received;
+				currentIn = down.readByte();
+			}
+			oldByteReceived = oldByteSend;
 		}
+
+		System.out.println("#######>"+this.hashCode()+"   DONE SENDING BYTE: "
+				+ Bytes.format(fullDataToSend));
+		connectionSetup = true;
 	}
 
 	@Override
 	public byte readByte() {
+		System.out.println("#######>"+this.hashCode()+"   START READING BYTE, oldReceivedByte = "
+				+ Bytes.format(oldByteReceived));
 		byte data = 0;
 
 		int bitIndex = 0;
@@ -79,38 +90,51 @@ public class HighSpeedHDXLinkLayer extends LinkLayer {
 
 			// Read and unpack a line byte
 			byte input = down.readByte();
-			
+			// System.out.println("Real Received:" + Bytes.format(input));
 
 			// If the clock has changed...
-			if (input != oldBitReceived) {
-				if(!connectionSetup){
+			if (input != oldByteReceived || !connectionSetup) {
+				if (!connectionSetup) {
 					input = 0;
-					//Treat first read byte always as 0, even if it isnt actually 0;
 					connectionSetup = true;
-				}
-				byte databit = (byte) (input & 1);
-				data |= databit << bitIndex;
-				bitIndex++;
-				// New data received;
-				// Insert the data at the correct location and flip the clock
-				
-				boolean databitChanged = databit != ((byte) (oldBitReceived & 1));
-				if(databitChanged){
-					//Read second bit
-					databit = (byte) (input & 2);
+					// Treat first read byte always as 0, even if it isnt
+					// actually 0;
+
+				} else {
+					connectionSetup = true;
+
+					byte extrabit = (byte) (input & 2);
+					byte databit = (byte) (input & 1);
 					data |= databit << bitIndex;
 					bitIndex++;
+					// New data received;
+					// Insert the data at the correct location and flip the
+					// clock
+
+					boolean databitChanged = databit != ((byte) (oldByteReceived & 1));
+					if (databitChanged) {
+						// Read second bit
+						data |= extrabit << bitIndex;
+						bitIndex++;
+					}
+
+					System.out.println("Current data received: ["+bitIndex+"]\t"
+							+ Bytes.format(data, bitIndex));
+					down.sendByte(input);
+					oldByteSend = input;
 				}
-
-				oldBitReceived = input;
-
-				// Send acknowledgement
-				down.sendByte(HardwareLayer.shuftLeft(input));
+				oldByteReceived = input;
 			}
 		}
 
-		// System.out.println("returning data " + Bytes.format(data));
+		System.out
+				.println("#######>"+this.hashCode()+"   DONE READING BYTE " + Bytes.format(data));
 		return data;
 	}
 
+	/**
+	 * Bits send: 9 Going to wait for ack Filtered Received:00000001 databit: 1
+	 * extrabit: 0 first databit, Current data received: 100000 Second databit,
+	 * Current data received: 0100000 Bits received: 7
+	 */
 }
--- src/util/Bytes.java
+++ src/util/Bytes.java
@@ -8,4 +8,10 @@ public abstract class Bytes {
 	public static String format(byte b) {
 		return String.format("%08d", Integer.parseInt(Integer.toBinaryString(b & 0xFF)));
 	}
+	/** Formats the given byte as an eight-character string. 
+	 * Only prints the given amount, starting at LSB;
+	 * */
+	public static String format(byte b,int amount) {
+		return String.format("%0"+amount+"d", Integer.parseInt(Integer.toBinaryString(b & 0xFF)));
+	}
 }
