5bada5323c351edd5d537960a778f5c7a7a52efe
Jan-Jelle Kester <janjelle@jjkester.nl>
2014-04-25 10:38:16 +0200

    Give the BenchmarkTestBench another try with a handshake

--- src/bench/BandwidthTestBench.java
+++ src/bench/BandwidthTestBench.java
@@ -14,16 +14,21 @@ public class BandwidthTestBench {
         if (args.length != 1 || !(args[0].equals("send") || args[0].equals("receive"))) {
             System.err.println("Invalid arguments.\nUse 'send' or 'receive'.");
         } else {
-            PhysicalLayer phys = new DumpingPhysicalLayer(new LptHardwareLayer());
+            PhysicalLayer phys = new LptHardwareLayer();
             LinkLayer link = new AckingLinkLayer(phys);
 
             System.out.println("STACK: " + link + "\n");
 
+            // Reset line
+            phys.sendByte((byte) 0);
+
             System.out.printf("Please wait %d seconds...\n\n", DURATION / 1000);
 
             if (args[0].equals("send")) {
-                int good = 0;
-                int bad = 0;
+                while(phys.readByte() != 0) {
+                    // Wait for handshake
+                }
+                phys.sendByte((byte) 3);
 
                 // Just spammin'
                 while (true) {
@@ -32,9 +37,15 @@ public class BandwidthTestBench {
                     }
                 }
             } else if (args[0].equals("receive")) {
-                long start, end;
+                long start;
                 int num = 0;
                 byte old = link.readByte();
+
+                // Handshake
+                while (phys.readByte() != 3) {
+                    // Wait for handshake.
+                }
+
                 start = System.currentTimeMillis();
 
                 while (System.currentTimeMillis() < start + DURATION) {
