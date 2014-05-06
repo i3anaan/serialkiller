4abf8172c2bceb2f5c75d4688ab39679ba483172
Jan-Jelle Kester <janjelle@jjkester.nl>
2014-05-06 11:32:46 +0200

    Use Wander's Layer implementation

--- src/common/Layer.java
+++ src/common/Layer.java
@@ -1,16 +1,10 @@
 package common;
 
 public abstract class Layer {
-	protected Layer down;
-	
-	/** Sends a single byte in a layer-defined way. */
-	public abstract void sendByte(byte data);
+    protected Layer down;
 
-	/** Reads a byte in a layer-defined way. */
-	public abstract byte readByte();
-	
-	/** Returns a string representation of this Layer and the ones below it. */
-	public String toString() {
-		return getClass().getSimpleName() + (down == null ? "" : "/" + down.toString());
-	}
+    /** Returns a string representation of this Layer and the ones below it. */
+    public String toString() {
+        return getClass().getSimpleName() + (down == null ? "" : "/" + down.toString());
+    }
 }
