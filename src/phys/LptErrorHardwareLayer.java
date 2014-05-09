1101083cd6b2ef3cf46df15fcd90bccdec5a7008
Snijders D.H.J. (Student) <s1299212@utwks10655.ewi.utwente.nl>
2014-05-09 14:33:30 +0200

    Add ErrorHardwareLayer

---
+++ src/phys/LptErrorHardwareLayer.java
@@ -0,0 +1,22 @@
+package phys;
+
+import lpt.Lpt;
+
+public class LptErrorHardwareLayer extends HardwareLayer {
+	private lpt.ErrorLpt lpt;
+
+	public LptErrorHardwareLayer() {
+		lpt = new lpt.ErrorLpt();
+	}
+
+	@Override
+	public void sendByte(byte data) {
+		lpt.writeLPT(data);
+	}
+
+	@Override
+	public byte readByte() {
+		return shuftRight(lpt.readLPT());
+	}
+
+}
