67af6a697d523e621cc60eae07a31ae8dac730e7
Wander Nauta <info@wandernauta.nl>
2014-05-20 14:02:03 +0200

    Add comments to Starter

--- src/starter/Starter.java
+++ src/starter/Starter.java
@@ -29,9 +29,15 @@ import link.BufferStufferLinkLayer;
 import log.LogMessage;
 import log.Logger;
 
+/**
+ * The Starter is responsible for starting complete instances of the
+ * Serialkiller stack, including the graphical interface and the web monitor
+ * service if required.
+ */
 public class Starter extends JFrame implements ActionListener {
 	private Logger log;
 	
+	// Options for all the combo boxes.
 	private String swingOptions[] = {"Yes", "No"};
 	private Class<?> applicationLayers[] = {ApplicationLayer.class};
 	private Class<?> networkLayers[] = {NetworkLayer.class};
@@ -39,22 +45,27 @@ public class Starter extends JFrame implements ActionListener {
 	private Class<?> physLayers[] = {LptHardwareLayer.class, LptErrorHardwareLayer.class, DummyPhysicalLayer.class};
 	private String webOptions[] = {"Yes", "No"};
 	
+	// Combo boxes for all the options.
 	private JComboBox swingCombo;
 	private JComboBox appCombo;
-	private JComboBox netCombo;
+	private JComboBox netCombo; 
 	private JComboBox linkCombo;
 	private JComboBox physCombo;
 	private JComboBox webCombo;
 	
+	// Main buttons.
 	private JButton start;
 	private JButton quit;
 	
+	/** Main entry point. This is the mainest main of all the mains. */
 	public static void main(String[] args) {
 		new Starter().run();
 	}
 	
 	public Starter() {
         log = new Logger(LogMessage.Subsystem.STARTER);
+        
+        // Build the combo's.
         swingCombo = combo(swingOptions);
         appCombo = combo(applicationLayers);
         netCombo = combo(networkLayers);
@@ -64,11 +75,13 @@ public class Starter extends JFrame implements ActionListener {
 
 		setLayout(new BorderLayout());
 		
+		// Put a descriptive label at the top.
 		String label = String.format("SerialKiller version %s. Pick your favorite implementations below.", Environment.getGitCommit());
 		JLabel topLabel = new JLabel(label);
 		topLabel.setBorder(new EmptyBorder(12, 12, 12, 12));
 		add(topLabel, BorderLayout.NORTH);
 		
+		// Put the main form in the center.
 		JPanel form = new JPanel();
 		form.setLayout(new GridLayout(0, 2, 12, 12));
 		form.add(label("Start Swing application?"));
@@ -84,9 +97,9 @@ public class Starter extends JFrame implements ActionListener {
 		form.add(label("Start web service?"));
 		form.add(webCombo);
 		form.setBorder(new EmptyBorder(12, 12, 12, 12));
-		
 		add(form, BorderLayout.CENTER);
 		
+		// Put the main buttons at the bottom.
 		JPanel buttons = new JPanel();
 		start = button("Start");
 		quit = button("Quit");
@@ -94,19 +107,23 @@ public class Starter extends JFrame implements ActionListener {
 		buttons.add(quit);
 		add(buttons, BorderLayout.SOUTH);
 		
+		// We've added everything we want.
 		pack();
 	}
 
+	/** Magically have the dialog appear out of thin air. */
 	public void run() {
 		setVisible(true);
 	}
 	
+	/** Does the actual stack-starting magic. */
 	private void startStack() {
 		log.info("Starter starting stack.");
 		log.debug("How many stacks would a stack starter start if a stack starter could start stacks?");
 		
 		// Instantiate and start the stack, bottom-up.
 		try {
+			// Physical layer.
 			log.info("Starting physical layer.");
 			Class<?> physClass = physLayers[physCombo.getSelectedIndex()];
 			log.info("Got class " + physClass);
@@ -115,9 +132,11 @@ public class Starter extends JFrame implements ActionListener {
 			phy.start();
 			log.info("Started " + phy);
 			
+			// Application layer.
 			log.info("Starting application layer.");
 			ApplicationLayer al = new ApplicationLayer();
 			
+			// Disable all controls except the Quit button.
 			swingCombo.setEnabled(false);
 			appCombo.setEnabled(false);
 			netCombo.setEnabled(false);
@@ -137,31 +156,36 @@ public class Starter extends JFrame implements ActionListener {
 				al.addObserver(gui);
 			}
 		} catch (InstantiationException e) {
+			// This should not happen.
 			log.emerg(e.toString());
 		} catch (IllegalAccessException e) {
+			// This should not happen at all ever.
 			log.emerg(e.toString());
 		}
 	}
 	
+	/** Make a new JButton and add ourselves as the ActionListener. */
 	private JButton button(String label) {
 		JButton out = new JButton(label);
 		out.addActionListener(this);
 		return out;
 	}
 	
+	/** Make a new JLabel. */
 	private JLabel label(String label) {
 		return new JLabel(label);
 	}
 	
+	/** Make a new JComboBox and add ourselves as the ActionListener. */
 	private JComboBox combo(Object[] items) {
 		return new JComboBox(items);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		if (event.getActionCommand().equals("Start")) {
+		if (event.getSource().equals(start)) {
 			startStack();
-		} else if (event.getActionCommand().equals("Quit")) {
+		} else if (event.getSource().equals(quit)) {
 			System.exit(0);
 		} else {
 			log.info("Received unknown action " + event);
