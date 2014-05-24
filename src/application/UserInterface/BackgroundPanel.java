06943fc116dcdd35452118bdbcad4a2ac14d7d7a
TheMSB <Martijn.bruning@gmail.com>
2014-05-24 16:37:41 +0200

    GUI fix

--- src/application/UserInterface/BackgroundPanel.java
+++ src/application/UserInterface/BackgroundPanel.java
@@ -4,80 +4,26 @@ import java.awt.image.*;
 import javax.swing.*;
 
 /**
- *  Support custom painting on a panel in the form of
- *
- *  a) images - that can be scaled, tiled or painted at original size
- *  b) non solid painting - that can be done by using a Paint object
- *
- *  Also, any component added directly to this panel will be made
- *  non-opaque so that the custom painting can show through.
+ *  Extension of JPanel where an image is used to 
+ *  draw a transparent background
  */
 public class BackgroundPanel extends JPanel
 {
-	public static final int SCALED = 0;
-	
-	private Paint painter;
 	private Image image;
-	private int style = SCALED;
-	
-	private boolean isTransparentAdd = true;
 
 	/**
-	 *  Set image as the background with the SCALED style
+	 *  Creates a new BackgroundPanel with the specified image as
+	 *  a background
 	 */
 	public BackgroundPanel(Image image)
 	{
-		this(image, SCALED);
-	}
-
-	/**
-	 *  Set image as the background with the specified style
-	 */
-	public BackgroundPanel(Image image, int style)
-	{
-		setImage( image );
-		setStyle( style );
-		setLayout( new BorderLayout() );
-	}
-
-	/**
-	 *  Use the Paint interface to paint a background
-	 */
-	public BackgroundPanel(Paint painter)
-	{
-		setPaint( painter );
-		setLayout( new BorderLayout() );
-	}
-
-	/**
-	 *	Set the image used as the background
-	 */
-	public void setImage(Image image)
-	{
 		this.image = image;
+		setLayout(new BorderLayout());
 		repaint();
 	}
 
 	/**
-	 *	Set the style used to paint the background image
-	 */
-	public void setStyle(int style)
-	{
-		this.style = style;
-		repaint();
-	}
-
-	/**
-	 *	Set the Paint object used to paint the background
-	 */
-	public void setPaint(Paint painter)
-	{
-		this.painter = painter;
-		repaint();
-	}
-
-	/**
-	 *  Override method so we can make the component transparent
+	 *  Override method to shorthand our add method with null constraint
 	 */
 	public void add(JComponent component)
 	{
@@ -85,104 +31,28 @@ public class BackgroundPanel extends JPanel
 	}
 
 	/**
-	 *  Override to provide a preferred size equal to the image size
-	 */
-	@Override
-	public Dimension getPreferredSize()
-	{
-		if (image == null)
-			return super.getPreferredSize();
-		else
-			return new Dimension(image.getWidth(null), image.getHeight(null));
-	}
-
-	/**
-	 *  Override method so we can make the component transparent
+	 *  Overrides the add method to make the added component transparent
 	 */
 	public void add(JComponent component, Object constraints)
 	{
-		if (isTransparentAdd)
-		{
-			makeComponentTransparent(component);
-		}
-
+		component.setOpaque(false);
 		super.add(component, constraints);
 	}
 
 	/**
-	 *  Controls whether components added to this panel should automatically
-	 *  be made transparent. That is, setOpaque(false) will be invoked.
-	 *  The default is set to true.
-	 */
-	public void setTransparentAdd(boolean isTransparentAdd)
-	{
-		this.isTransparentAdd = isTransparentAdd;
-	}
-
-	/**
-	 *	Try to make the component transparent.
-	 *  For components that use renderers, like JTable, you will also need to
-	 *  change the renderer to be transparent. An easy way to do this it to
-	 *  set the background of the table to a Color using an alpha value of 0.
-	 */
-	private void makeComponentTransparent(JComponent component)
-	{
-		component.setOpaque( false );
-
-		if (component instanceof JScrollPane)
-		{
-			JScrollPane scrollPane = (JScrollPane)component;
-			JViewport viewport = scrollPane.getViewport();
-			viewport.setOpaque( false );
-			Component c = viewport.getView();
-
-			if (c instanceof JComponent)
-			{
-				((JComponent)c).setOpaque( false );
-			}
-		}
-	}
-
-	/**
-	 *  Add custom painting
+	 *  Modifies the painter of this component to draw a background image
 	 */
 	@Override
 	protected void paintComponent(Graphics g)
 	{
+		// Call the original painter
 		super.paintComponent(g);
 
-		//  Invoke the painter for the background
-
-		if (painter != null)
-		{
-			Dimension d = getSize();
-			Graphics2D g2 = (Graphics2D) g;
-			g2.setPaint(painter);
-			g2.fill( new Rectangle(0, 0, d.width, d.height) );
-		}
-
-		//  Draw the image
-
-		if (image == null ) return;
-
-		switch (style)
-		{
-			case SCALED :
-				drawScaled(g);
-				break;
-
-			default:
-            	drawScaled(g);
-		}
-	}
-
-	/**
-	 *  Custom painting code for drawing a SCALED image as the background
-	 */
-	private void drawScaled(Graphics g)
-	{
+		//  Draws the image if there is one
+		if (image != null ){
 		Dimension d = getSize();
 		g.drawImage(image, 0, 0, d.width, d.height, null);
+		}
 	}
 
 }
--- src/application/UserInterface/ClosableTabbedPane.java
+++ src/application/UserInterface/ClosableTabbedPane.java
@@ -14,43 +14,84 @@ import javax.swing.*;
 
 
 public class ClosableTabbedPane extends JTabbedPane{
+	
+	/** The UI for drawing the close button */
 	private TabCloseUI closeUI = new TabCloseUI(this);
 	
+	/** override of the tabbedPane paint method to
+	 * also draw the close button UI
+	 */
 	public void paint(Graphics g){
 		super.paint(g);
 		closeUI.paint(g);
 	}
 	
+	/**
+	 * Add a tab to the pane with added spacing
+	 */
 	public void addTab(String title, Component component) {
-		super.addTab(title+"  ", component);
+		super.addTab(title+"    ", component);
 	}
 	
-	
+	/**
+	 * Method to return the title of a tab
+	 * @param index of the tab
+	 * @return title of the tab
+	 */
 	public String getTabTitleAt(int index) {
 		return super.getTitleAt(index).trim();
 	}
 	
 	private class TabCloseUI implements MouseListener, MouseMotionListener {
+		
+		/** the tabbed pane being that is used by this instance */
 		private ClosableTabbedPane  tabbedPane;
+		
+		/** position of the mouse and tab */
 		private int closeX = 0 ,closeY = 0, meX = 0, meY = 0;
+	
+		/** the currently selected tab */
 		private int selectedTab;
-		private final int  width = 5, height = 5;
+		
+		/** the width of the close UI */
+		private final int  width = 5;
+		
+		/** the height of the close UI */
+		private final int  height = 5;
+		/** selection rectangle for the close UI */
 		private Rectangle rectangle = new Rectangle(0,0,width, height);
-		private TabCloseUI(){}
+		
 		public TabCloseUI(ClosableTabbedPane pane) {
 			
 			tabbedPane = pane;
 			tabbedPane.addMouseMotionListener(this);
 			tabbedPane.addMouseListener(this);
 		}
-		public void mouseEntered(MouseEvent me) {}
-		public void mouseExited(MouseEvent me) {}
-		public void mousePressed(MouseEvent me) {}
-		public void mouseClicked(MouseEvent me) {}
-		public void mouseDragged(MouseEvent me) {}
+		
+		/** mouse events */
+		@Override 
+		public void mouseEntered(MouseEvent me) {
+			// Nothing
+		}
+		@Override
+		public void mouseExited(MouseEvent me) {
+			// Nothing
+		}
+		@Override
+		public void mousePressed(MouseEvent me) {
+			// Nothing
+		}
+		@Override
+		public void mouseClicked(MouseEvent me) {
+			// Nothing
+		}
+		@Override
+		public void mouseDragged(MouseEvent me) {
+			// Nothing
+		}
 		
 		
-
+		/** event when close button has been clicked and mouse has been released */
 		public void mouseReleased(MouseEvent me) {
 			if(closeUnderMouse(me.getX(), me.getY())){
 				boolean isToCloseTab = tabAboutToClose(selectedTab);
@@ -85,12 +126,24 @@ public class ClosableTabbedPane extends JTabbedPane{
 				}	
 		}
 
+		/**
+		 * Method to close a tab when a close button is clicked
+		 * during a mouse over event
+		 * @param x coordinate of the mouse
+		 * @param y coordinate of the mouse
+		 * @return boolean
+		 */
 		private boolean closeUnderMouse(int x, int y) {		
 			rectangle.x = closeX;
 			rectangle.y = closeY;
 			return rectangle.contains(x,y);
 		}
 
+		/**
+		 * Main draw method of the close UI for 
+		 * the tabbedPane
+		 * @param Graphics component
+		 */
 		public void paint(Graphics g) {
 			
 			int tabCount = tabbedPane.getTabCount();
@@ -106,18 +159,35 @@ public class ClosableTabbedPane extends JTabbedPane{
 			}
 		}
 
+		/**
+		 * Method to draw the close button
+		 * @param Graphic component
+		 * @param x coordinate of the mouse
+		 * @param y coordinate of the mouse
+		 */
 		private void drawClose(Graphics g, int x, int y) {
 			if(tabbedPane != null && tabbedPane.getTabCount() > 0){
 				Graphics2D g2 = (Graphics2D)g;				
-				drawColored(g2, isUnderMouse(x,y)? Color.RED : Color.WHITE, x, y);
+				drawColored(g2, isUnderMouse(x,y)? Color.DARK_GRAY : Color.GRAY, x, y);
 			}
 		}
 
+		/**
+		 * Core method to draw the close button
+		 * @param Graphics2D component
+		 * @param color of the button
+		 * @param x coordinate where to draw
+		 * @param y coordinate where to draw
+		 */
 		private void drawColored(Graphics2D g2, Color color, int x, int y) {
+			
+			// Draw outline of close button by creating a bigger button under it
 			g2.setStroke(new BasicStroke(5,BasicStroke.JOIN_ROUND,BasicStroke.CAP_ROUND));
-			g2.setColor(Color.BLACK);
+			g2.setColor(Color.WHITE);
 			g2.drawLine(x, y, x + width, y + height);
 			g2.drawLine(x + width, y, x, y + height);
+			
+			// Draw the inner part of the close button by drawing a smaller button on top
 			g2.setColor(color);
 			g2.setStroke(new BasicStroke(3, BasicStroke.JOIN_ROUND, BasicStroke.CAP_ROUND));
 			g2.drawLine(x, y, x + width, y + height);
@@ -125,12 +195,25 @@ public class ClosableTabbedPane extends JTabbedPane{
 
 		}
 
+		/**
+		 * Method to check if the tab is located under
+		 * the mouse pointer
+		 * @param x coordinate of the mouse
+		 * @param y coordinate of the mouse
+		 * @return true if under mouse else false
+		 */
 		private boolean isUnderMouse(int x, int y) {
 			if(Math.abs(x-meX)<width && Math.abs(y-meY)<height )
 				return  true;		
 			return  false;
 		}
 
+		/**
+		 * Method for drawing the close button at a mouse over event
+		 * @param x coordinate of the tab
+		 * @param y coordinate of the tab
+		 * @return
+		 */
 		private boolean mouseOverTab(int x, int y) {
 			int tabCount = tabbedPane.getTabCount();
 			for(int j = 0; j < tabCount; j++)
@@ -145,6 +228,12 @@ public class ClosableTabbedPane extends JTabbedPane{
 
 	}
 
+	/**
+	 * Actions to perform when a tab is about to close,
+	 * use this to create confirmation dialogs or exit code
+	 * @param index of the closing tab
+	 * @return success of closure
+	 */
 	public boolean tabAboutToClose(int tabIndex) {
 		return true;
 	}
