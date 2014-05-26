5deda1d1bc99872822ce2d388d814a4a5f80e41e
Wander Nauta <info@wandernauta.nl>
2014-05-26 21:23:24 +0200

    Clean up MockLinkLayerTest

--- src/test/unit/MockLinkLayerTest.java
+++ src/test/unit/MockLinkLayerTest.java
@@ -3,24 +3,17 @@ package test.unit;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
-
 import link.diag.MockLinkLayer;
-
 import org.junit.Test;
-
 import com.google.common.base.Charsets;
-
 import common.Stack;
 
 public class MockLinkLayerTest {
 
 	@Test
 	public void test() {
-		Stack a = new Stack();
-		Stack b = new Stack();
-		
-		MockLinkLayer mla = new MockLinkLayer();
-		MockLinkLayer mlb = new MockLinkLayer();
+		Stack a = new Stack(), b = new Stack();
+		MockLinkLayer mla = new MockLinkLayer(), mlb = new MockLinkLayer();
 		
 		a.linkLayer = mlb;
 		b.linkLayer = mla;
@@ -31,10 +24,7 @@ public class MockLinkLayerTest {
 		byte[] test = "Testing!".getBytes(Charsets.UTF_8);
 		
 		mla.sendFrame(test);
-		
-		byte[] out = mlb.readFrame();
-		
-		assertTrue(Arrays.equals(test, out));
+		assertTrue(Arrays.equals(test, mlb.readFrame()));
 	}
 
 }
