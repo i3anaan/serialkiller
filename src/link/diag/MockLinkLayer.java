82608eb2e01f0d562bdfb86cab47395693b6efaf
Wander Nauta <info@wandernauta.nl>
2014-05-26 21:15:43 +0200

    Make MockLinkLayer a FrameLinkLayer

--- src/link/diag/MockLinkLayer.java
+++ src/link/diag/MockLinkLayer.java
@@ -3,68 +3,44 @@ package link.diag;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
-import util.Bytes;
+import link.FrameLinkLayer;
 
 /**
  * A fake implementation of a link layer that only provides communication
  * between threads, not between hosts.
  * 
- * Both readByte and sendByte potentially block, with readByte blocking being
+ * Both readFrame and sendFrame potentially block, with readFrame blocking being
  * much more likely.
  */
-public class MockLinkLayer extends BytewiseLinkLayer {
+public class MockLinkLayer extends FrameLinkLayer {
 	private static final int queue_sz = 1024;
-
 	private MockLinkLayer that;
-	private BlockingQueue<Byte> bq;
-	private String prefix = ">";
-	private boolean debug = false;
+	private BlockingQueue<byte[]> bq;
 
 	/** Connects this MockLinkLayer to a peer. */
 	public MockLinkLayer connect(MockLinkLayer that) {
 		this.that = that;
-		this.bq = new ArrayBlockingQueue<Byte>(queue_sz);
-		return this;
-	}
-
-	/** Sets the prefix this MockLinkLayer uses while debug logging. */
-	public MockLinkLayer setPrefix(String prefix) {
-		this.prefix = prefix;
-		return this;
-	}
-
-	/** Enable or disable debug logging. */
-	public MockLinkLayer setDebug(boolean debug) {
-		this.debug = debug;
+		this.bq = new ArrayBlockingQueue<byte[]>(queue_sz);
 		return this;
 	}
 
 	@Override
-	public void sendByte(byte data) {
-		if (that != null)
-			that.push(data);
-
-		if (debug) {
-			String fmt = "[MLL] sent %s '%s' %s %s\n";
-			String chr = "" + ((char) data);
-			String val = Byte.toString(data);
-
-			System.out.printf(fmt, prefix, chr, val, Bytes.format(data));
-		}
+	public void sendFrame(byte[] data) {
+		if (that != null) that.push(data);
 	}
 
 	@Override
-	public byte readByte() {
+	public byte[] readFrame() {
 		try {
 			return bq.take();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-			return (byte) 0;
+			return null;
 		}
 	}
 
-	/** Add a byte of data to be consumed by the owner of this LinkLayer. */
-	public void push(byte data) {
+	/** Add a byte array of data to be consumed by the owner of this Layer. */
+	public void push(byte[] data) {
 		try {
 			bq.put(data);
 		} catch (InterruptedException e) {
