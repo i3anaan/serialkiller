59a8f899a70740c33d62beefa21f99a4efa7d7a3
Jan-Jelle Kester <janjelle@jjkester.nl>
2014-06-03 15:07:50 +0200

    Actually handle data from the network layer

--- src/network/tpp/TPPNetworkLayer.java
+++ src/network/tpp/TPPNetworkLayer.java
@@ -296,6 +296,7 @@ public class TPPNetworkLayer extends NetworkLayer implements Runnable {
                     sendPacket(ack);
                     routerLock.unlock();
                     TPPNetworkLayer.getLogger().debug("Sent acknowledgement for " + p.toString() + ": " + ack.toString() + ".");
+                    sendPacket(p);
                 } else {
                     if (!inRoute.containsKey(p.header().getDestination())) {
                         inRoute.put(p.header().getDestination(), 0);
