d29d8afcced720877170781afafecb238dfb9f41
Wander Nauta <info@wandernauta.nl>
2014-06-20 16:17:01 +0200

    Make encode and decode static

--- src/link/angelmaker/codec/ParityBitsCodec.java
+++ src/link/angelmaker/codec/ParityBitsCodec.java
@@ -7,7 +7,7 @@ import util.BitSet2;
 /**
  * A Codec that implements a two-bits-per-byte parity check.
  */
-public class ParityBitsCodec implements Codec {
+public class ParityBitsCodec {
 
 	/**
 	 * Encodes a BitSet of input data by adding two parity bits for every
@@ -15,8 +15,7 @@ public class ParityBitsCodec implements Codec {
 	 * 
 	 * @throws IllegalArgumentException if the BitSet is not a BitSet of bytes
 	 */
-	@Override
-	public BitSet2 encode(BitSet2 input) throws IllegalArgumentException {
+	public static BitSet2 encode(BitSet2 input) throws IllegalArgumentException {
 		if (input.length() % 8 != 0) {
 			String msg = "encode: input.length() must be a multiple of 8 bits";
 			throw new IllegalArgumentException(msg);
@@ -43,8 +42,7 @@ public class ParityBitsCodec implements Codec {
 	 * 
 	 * @throws IllegalArgumentException if the BitSet is not encoded parity
 	 */
-	@Override
-	public Optional<BitSet2> decode(BitSet2 input) throws IllegalArgumentException {
+	public static Optional<BitSet2> decode(BitSet2 input) throws IllegalArgumentException {
 		if (input.length() % 10 != 0) {
 			String msg = "decode: input.length() must be a multiple of 10 bits";
 			throw new IllegalArgumentException(msg);
